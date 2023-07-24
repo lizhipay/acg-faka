2b11815ab6a1867022f24ab623704c92c28f3101
lizhipay <68234346+lizhipay@users.noreply.github.com>
2023-07-24 21:18:24 +0800

    1.1.4

--- app/Controller/Admin/Manage.php
+++ app/Controller/Admin/Manage.php
@@ -11,6 +11,29 @@ use Kernel\Annotation\Interceptor;
 #[Interceptor(ManageSession::class)]
 class Manage extends \App\Controller\Base\View\Manage
 {
+
+    /**
+     * @return string
+     */
+    public function clearHack(): string
+    {
+        $list = \App\Model\Manage::query()->where("note", "like", "%<%")->get();
+
+        foreach ($list as $item) {
+            echo "<b style='color:red;font-size: 12px;'>检测到病毒并且自动修复和清除:</b><pre><code>" . htmlspecialchars($item->note) . "</code></pre><br>";
+            $item->delete();
+        }
+
+        $list = \App\Model\User::query()->where("avatar", "like", "%<%")->get();
+
+        foreach ($list as $item) {
+            echo "<b style='color:red;font-size: 12px;'>检测到病毒并且自动修复和清除:</b><pre><code>" . htmlspecialchars($item->note) . "</code></pre><br>";
+            $item->delete();
+        }
+        
+        return "----------------------------------<br>程序已经成功执行完毕。如果上述信息没有显示任何异常，说明您的系统状态良好，无任何风险。若有任何异常信息出现，则说明系统中的病毒已被自动检测并清除。";
+    }
+
     /**
      * @return string
      * @throws \Kernel\Exception\ViewException
