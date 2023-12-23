23b48e96513a0f77ee18d54bc70598c37144c064
lizhipay <chunxi1937@gmail.com>
2023-12-23 23:12:11 +0800

    1.2.0

--- app/Controller/Admin/Manage.php
+++ app/Controller/Admin/Manage.php
@@ -21,7 +21,8 @@ class Manage extends \App\Controller\Base\View\Manage
 
         foreach ($list as $item) {
             echo "<b style='color:red;font-size: 12px;'>检测到病毒并且自动修复和清除:</b><pre><code>" . htmlspecialchars((string)$item->avatar) . "</code></pre><br>";
-            $item->delete();
+            $item->avatar = "";
+            $item->save();
         }
 
         $list = \App\Model\User::query()->where("avatar", "like", "%\"%")->get();
@@ -30,7 +31,7 @@ class Manage extends \App\Controller\Base\View\Manage
             echo "<b style='color:red;font-size: 12px;'>检测到病毒并且自动修复和清除:</b><pre><code>" . htmlspecialchars((string)$item->avatar) . "</code></pre><br>";
             $item->delete();
         }
-        
+
         return "----------------------------------<br>程序已经成功执行完毕。如果上述信息没有显示任何异常，说明您的系统状态良好，无任何风险。若有任何异常信息出现，则说明系统中的病毒已被自动检测并清除。";
     }
 
