1e152b7b982e813df2806f1e0e08514d8f46ab31
lizhipay <chunxi1937@gmail.com>
2023-12-24 00:23:40 +0800

    1.2.1

--- app/Util/Waf.php
+++ app/Util/Waf.php
@@ -26,7 +26,7 @@ class Waf
         }
 
         foreach ($_POST as $key => $value) {
-            if (str_contains((string)$value, "=")) {
+            if ((str_contains((string)$value, "=") && str_contains((string)$value, "\"")) || str_contains((string)$value, "=") && str_contains((string)$value, "'")) {
                 throw new JSONException("WAF检测，请勿使用HTML语法");
             }
         }
